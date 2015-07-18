286b5d640668ff9e608cb5cbe43e745cc5b7b24e
BlaiseGratton <blaisegratton@gmail.com>
2015-07-18 14:05:03 -0500

    initialised console app

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayAdding
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a positive integer: ");
+            int upperLimit = int.Parse(Console.ReadLine());
+            
+            List<int> evenIntegers = new List<int>();
+            
+            for (var i = 1; i < upperLimit; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenIntegers.Add(i);
+                    Console.WriteLine(i);
+                }
+            }
+            
+            
+        }
+    }
+}
